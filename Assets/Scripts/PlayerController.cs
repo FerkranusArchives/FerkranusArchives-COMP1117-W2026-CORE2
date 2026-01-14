6c4aeffb2389d3acc13c72ab8c99864093165091
Emma Ferkranus <emmaferk@gmail.com>
2026-01-14 13:41:38 -0500

    Added PlayerController starter script from class repo week 1 as base for my work this week, added TestEnemy script code from my notes in week 2's class

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerStats stats;
+
+    // Components
+    private Rigidbody2D rBody;
+
+    // Field variables
+    private Vector2 moveInput;
+
+    void Awake()
     {
-        
+        // Initialize
+        rBody = GetComponent<Rigidbody2D>();
+
+        stats = new PlayerStats();
+        int something = stats.MoveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue value)
     {
-        
+        moveInput = value.Get<Vector2>();
     }
-}
+
+    void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    void ApplyMovement()
+    {
+        float velocityX = moveInput.x;
+
+        rBody.linearVelocity = new Vector2(velocityX, rBody.linearVelocity.y);
+    }
+    }
